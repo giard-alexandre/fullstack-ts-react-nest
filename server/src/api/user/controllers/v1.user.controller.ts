31fc6e5debfc7bab1b76a67df0c9438f9db10824
Alexandre Giard <agiard@drw.com>
2019-08-30 13:49:47 -0400

    Format UserController

--- server/src/api/user/controllers/v1.user.controller.ts
+++ server/src/api/user/controllers/v1.user.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { Observable, of } from 'rxjs';
 import { User } from '@shared/entities/user/user.entity';
+import { Observable, of } from 'rxjs';
 import { UserService } from '../services/user.service';
 
 @Controller('v1/user')
 export class V1UserController {
-    constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) {
+    }
 
     @Get()
     getTasks(): Observable<User[]> {
